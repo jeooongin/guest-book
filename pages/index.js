aa9f612e929dc73f8e1b9d043e131f8a1265723c
jeongin <kwjddls1919@naver.com>
2022-11-22 00:11:22 +0900

    Refactor: 코드 정리

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import GuestBookCard from "../components/GuestBookCard";
 
 import GuestBookForm from "../components/GuestBookForm";
@@ -11,11 +10,6 @@ const index = () => {
       <GuestBookCard />
     </>
   );
-=======
-
-const index = () => {
-  return <div>index</div>;
->>>>>>> 1b49230188ddc17abc5d46f54e3794999b19361b
 };
 
 export default index;
