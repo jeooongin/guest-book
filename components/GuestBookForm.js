a0c9bb2e221214928b2e31e6594471d7d39ec002
jeongin <kwjddls1919@naver.com>
2022-11-22 00:44:05 +0900

    Design: 디자인 변경

--- components/GuestBookForm.js
+++ components/GuestBookForm.js
@@ -1,19 +1,43 @@
 import { Button, Form, Input } from "antd";
-import React from "react";
+import React, { useCallback } from "react";
+import { useDispatch } from "react-redux";
+
+import { addGuestBook } from "../reducers/guestBook";
 
 const GuestBookForm = () => {
+  const dispatch = useDispatch();
+
+  const onSubmitForn = useCallback(() => {
+    dispatch(addGuestBook());
+  }, []);
+
   return (
-    <Form>
+    <Form
+      onFinish={onSubmitForn}
+      style={{
+        padding: "10px",
+        borderRadius: "10px",
+        background: "white",
+      }}
+    >
+      <label htmlFor="guest-book">방명록</label>
       <Input.TextArea rows={4} placeholder="방명록을 남겨주세요." />
+      <div style={{ marginTop: "5px" }}>
+        <label htmlFor="name">이름</label>
+        <Input rows={1} type="text" placeholder="이름을 입력해주세요." />
+        <label htmlFor="password">비밀번호</label>
+        <Input rows={1} type="password" placeholder="비밀번호를 입력해주세요" />
+      </div>
       <div
         style={{
           display: "flex",
           justifyContent: "flex-end",
           marginTop: "5px",
-          marginBottom: "10px",
         }}
       >
-        <Button type="primary">등록</Button>
+        <Button type="primary" htmlType="submit">
+          등록
+        </Button>
       </div>
     </Form>
   );
