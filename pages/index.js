9a36d27a3a1b5b3a51f23f5416eb908023f88c9c
jeongin <kwjddls1919@naver.com>
2022-11-22 00:01:15 +0900

    Feat: index 페이지 생성

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import GuestBookCard from "../components/GuestBookCard";
+
+import GuestBookForm from "../components/GuestBookForm";
+
+const index = () => {
+  return (
+    <>
+      <GuestBookForm />
+      <GuestBookCard />
+    </>
+  );
+};
+
+export default index;
