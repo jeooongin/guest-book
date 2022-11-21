00c4c948fe158549d092269b3a45541b9fbc77b9
jeongin <kwjddls1919@naver.com>
2022-11-22 00:10:49 +0900

    Refactor: 불필요한 import 제거

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,9 @@
 import Head from "next/head";
 import React from "react";
-<<<<<<< HEAD
 import "antd/dist/reset.css";
 
 import App from "../components/App";
 import wrapper from "../store/configureStore";
-=======
-import App from "../components/App";
->>>>>>> 1b49230188ddc17abc5d46f54e3794999b19361b
 
 const _app = ({ Component }) => {
   return (
@@ -22,8 +18,4 @@ const _app = ({ Component }) => {
   );
 };
 
-<<<<<<< HEAD
 export default wrapper.withRedux(_app);
-=======
-export default _app;
->>>>>>> 1b49230188ddc17abc5d46f54e3794999b19361b
