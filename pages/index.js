9419f7750385bfef3592798232826afc30b06fd3
jeongin <kwjddls1919@naver.com>
2022-11-22 02:03:29 +0900

    Feat: GuestBookCard 반복 생성 추가

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import GuestBookCard from "../components/GuestBookCard";
 
 import GuestBookForm from "../components/GuestBookForm";
 
 const index = () => {
+  const { guestBooks } = useSelector((state) => state.guestBook);
   return (
     <>
       <GuestBookForm />
-      <GuestBookCard />
+      {guestBooks.map((guestBook) => (
+        <GuestBookCard key={guestBooks.id} guestBook={guestBook} />
+      ))}
     </>
   );
 };
