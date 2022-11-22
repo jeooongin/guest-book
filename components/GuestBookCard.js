f6c7b434fc504adbab674531275be968faa73076
jeongin <kwjddls1919@naver.com>
2022-11-22 10:05:29 +0900

    Feat: 방명록 데이터를 보여줄 기능 추가

--- components/GuestBookCard.js
+++ components/GuestBookCard.js
@@ -1,8 +1,12 @@
 import { Card } from "antd";
 import React from "react";
 
-const GuestBookCard = () => {
-  return <Card></Card>;
+const GuestBookCard = ({ guestBook }) => {
+  return (
+    <Card title={guestBook.name} style={{ marginTop: "10px" }}>
+      {guestBook.content}
+    </Card>
+  );
 };
 
 export default GuestBookCard;
