12e07b8f9e3cf11a8c45c287115a4fdc6a49a098
jeongin <kwjddls1919@naver.com>
2022-11-24 13:10:55 +0900

    Feat: 방명록 카드에 삭제 버튼 추가

--- components/GuestBookCard.js
+++ components/GuestBookCard.js
@@ -1,9 +1,23 @@
-import { Card } from "antd";
+import { Button, Card, Popover } from "antd";
 import React from "react";
+
+import GuestBookDeleteForm from "./GuestBookDeleteForm";
 
 const GuestBookCard = ({ guestBook }) => {
   return (
-    <Card title={guestBook.name} style={{ marginTop: "10px" }}>
+    <Card
+      title={guestBook.name}
+      extra={
+        <Popover
+          content={<GuestBookDeleteForm />}
+          title="패스워드를 입력해주세요."
+          trigger="click"
+        >
+          <Button>삭제</Button>
+        </Popover>
+      }
+      style={{ marginTop: "10px" }}
+    >
       {guestBook.content}
     </Card>
   );
